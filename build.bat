dc364be8df9cee8362d083e99c11219a2d4acc18
GatoArtStudio <hackhcctecno@gmail.com>
2024-06-19 16:58:17 -0500

    Actualizando el script de compilacion

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 setlocal
-set BUILD=24
+set BUILD=25
 set VERSION="1.0.%BUILD%.0"
 set PRODUCT="Kailand V - Launcher"
 set COPYRIGHT="Launcher de Kailand, by GatoArtStudio"
@@ -7,5 +7,5 @@ set DESCRIPTION="Launcher Oficial de Kailand V"
 set COMPANY="By GatoArtStudio X Kailand V"
 
 flet build windows -v --build-number %BUILD% --build-version %VERSION% --product %PRODUCT% --copyright %COPYRIGHT% --description %DESCRIPTION% --company %COMPANY%
-flet build linux -v --build-number %BUILD% --build-version %VERSION% --product %PRODUCT% --copyright %COPYRIGHT% --description %DESCRIPTION% --company %COMPANY%
-flet build macos -v --build-number %BUILD% --build-version %VERSION% --product %PRODUCT% --copyright %COPYRIGHT% --description %DESCRIPTION% --company %COMPANY%
+@REM flet build linux -v --build-number %BUILD% --build-version %VERSION% --product %PRODUCT% --copyright %COPYRIGHT% --description %DESCRIPTION% --company %COMPANY%
+@REM flet build macos -v --build-number %BUILD% --build-version %VERSION% --product %PRODUCT% --copyright %COPYRIGHT% --description %DESCRIPTION% --company %COMPANY%
