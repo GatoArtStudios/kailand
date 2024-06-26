ea15ff06873790104a8ffdbe2a5b82f8ffa22ec9
GatoArtStudio <hackhcctecno@gmail.com>
2024-06-25 20:41:43 -0500

    Creando un nuevo contructor de codigo

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+cd src
 setlocal
 set BUILD=25
 set VERSION="1.0.%BUILD%.1"
@@ -6,6 +7,6 @@ set COPYRIGHT="Launcher de Kailand, by GatoArtStudio"
 set DESCRIPTION="Launcher Oficial de Kailand V"
 set COMPANY="By GatoArtStudio X Kailand V"
 
-flet build windows -v --build-number %BUILD% --build-version %VERSION% --product %PRODUCT% --copyright %COPYRIGHT% --description %DESCRIPTION% --company %COMPANY%
+flet build windows -v --build-number %BUILD% --build-version %VERSION% --product %PRODUCT% --copyright %COPYRIGHT% --description %DESCRIPTION% --company %COMPANY% -o "C:\Users\%USERNAME%\Documents\GitHub\kailand\build"
 @REM flet build linux -v --build-number %BUILD% --build-version %VERSION% --product %PRODUCT% --copyright %COPYRIGHT% --description %DESCRIPTION% --company %COMPANY%
 @REM flet build macos -v --build-number %BUILD% --build-version %VERSION% --product %PRODUCT% --copyright %COPYRIGHT% --description %DESCRIPTION% --company %COMPANY%
