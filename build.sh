c18b6dc091dc1e4e1d7553414c45d002088cc3a5
GatoArtStudio <hackhcctecno@gmail.com>
2024-06-26 08:45:15 -0500

    Solucionando problemas con el compilador y las rutas de directorios

--- build.sh
+++ build.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+# Definir colores
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # Sin color
+
 # Ingresa a la carpeta de contruccion
 
-cd src | echo 'Entrando en carpeta de construccion' || echo 'No se encontro la carpeta de construccion'
-
-rm -vr /minecraft_launcher_cunstom/__pycache__/ | echo 'Eliminando carpeta de cache' || echo 'No hay carpetas de cache'
-rm -vr /minecraft_launcher_cunstom/_internal_types/__pycache__/ | echo 'Eliminando carpeta de cache' || echo 'No hay carpetas de cache'
-rm -vr /__pycache__/ | echo 'Eliminando carpeta de cache' || echo 'No hay carpetas de cache'
+rm -vr $PWD/src/minecraft_launcher_cunstom/__pycache__ | echo -e "${GREEN}Eliminando carpeta de cache${NC}" || echo -e "${RED}No hay carpetas de cache${NC}"
+rm -vr $PWD/src/minecraft_launcher_cunstom/_internal_types/__pycache__ | echo -e "${GREEN}Eliminando carpeta de cache${NC}" || echo -e "${RED}No hay carpetas de cache${NC}"
+rm -vr $PWD/src/__pycache__ | echo -e "${GREEN}Eliminando carpeta de cache${NC}" || echo -e "${RED}No hay carpetas de cache${NC}"
 
 BUILD=2
-VERSION='1.0.25'
-PRODUCT='Kailand'
+VERSION="1.0.25"
+PRODUCT="Kailand"
 COPYRIGHT="Launcher de Kailand, by GatoArtStudio"
 DESCRIPTION="Launcher Oficial de Kailand V"
 COMPANY="By GatoArtStudio X Kailand V"
-OUTPUT='/home/gatoartstudio/tools/kailand'
+OUTPUT="/home/${USER}/tools/kailand"
 
-echo 'Argumentos de compilacion son ' $@
+echo -e "${GREEN}Argumentos de compilacion son $BUILD $VERSION $PRODUCT $COPYRIGHT $DESCRIPTION $COMPANY $OUTPUT${NC}"
 
-flet build linux -v --build-number $BUILD --build-version $VERSION --product $PRODUCT --copyright $COPYRIGHT --description $DESCRIPTION --company $COMPANY -o $OUTPUT
+cd src && flet build linux -v --build-number $BUILD --build-version "$VERSION" --product "$PRODUCT" --copyright "$COPYRIGHT" --description "$DESCRIPTION" --company "$COMPANY" -o "$OUTPUT"
