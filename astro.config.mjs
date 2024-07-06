cb1c61f2f70e061f6c10e8d1599ccb336cd88813
GatoArtStudio <hackhcctecno@gmail.com>
2024-07-06 11:56:36 -0500

    Agregando mentadatos para SEO y automatizacion para produccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    load: [
+        async () => ({
+            base: process.env.NODE_ENV === 'production' ? '/kailand/' : '/',
+            site: {
+                keywords: ['kailand', 'sitio', 'web', 'minecraft', 'kailand v', 'server'],
+                url: 'https://www.kailand.es',
+            },
+        })
+    ]
+});
