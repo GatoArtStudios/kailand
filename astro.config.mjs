7d3778db8ac07030d4aad391c2d91cd09dbf54cc
GatoArtStudio <hackhcctecno@gmail.com>
2024-07-16 22:36:30 -0500

    Agregando barra de navegacion y redes \(sin terminar\)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    load: [
-        async () => ({
-            base: process.env.NODE_ENV === 'production' ? '/kailand/' : '/',
-            site: {
-                keywords: ['kailand', 'sitio', 'web', 'minecraft', 'kailand v', 'server'],
-                url: 'https://www.kailand.es',
-            },
-        })
-    ]
+  load: [async () => ({
+    base: process.env.NODE_ENV === 'production' ? '/kailand/' : '/',
+    site: {
+      keywords: ['kailand', 'sitio', 'web', 'minecraft', 'kailand v', 'server'],
+      url: 'https://www.kailand.es'
+    }
+  })],
+  integrations: [tailwind()]
 });
