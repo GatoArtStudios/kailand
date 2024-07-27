dbb9da587ec6ef406c2a463a8f54052a0919ee99
GatoArtStudio <hackhcctecno@gmail.com>
2024-07-27 10:34:40 -0500

    Agregando informacion en launcher y en comunicados

--- src/components/Comunicados.jsx
+++ src/components/Comunicados.jsx
@@ -1,9 +1,43 @@
 import TitleWiki from "./react/TitleWiki";
+import ParLeft from "./react/ParLeft";
+import SubTitleWiki from "./react/SubTitleWiki";
 
 export default function Comunicados() {
     return (
         <div className="flex flex-col justify-center items-center">
-            <TitleWiki text="Comunicados" />
+            <TitleWiki text="Sobre el servidor de Minecraft" />
+            <ParLeft>
+                <li className="my-5">
+                El servidor de Minecraft (Kailand), es un juego. Por lo tanto, las peleas, disputas y discusiones dentro del servidor, se quedan dentro del servidor. No se tolerará toxicidad ni faltas de respeto, por motivos relacionados al juego.
+                </li>
+                <li className="my-5">
+                Se recomienda a los jugadores que, usen un teclado 100% para más comodidad, debido a que, el servidor contiene muchos mods con atajos de teclado o teclas que asignar para usar los mods.
+                </li>
+                <li className="my-5">
+                Las personas que le digan a algún administrador, un bug, serán recompensados.
+                </li>
+                <li className="my-5">
+                El servidor es un poco gore. Por lo tanto, no nos hacemos responsables del impacto que tenga esto en los usuarios.
+                </li>
+                <li className="my-5">
+                Se recomienda NO utilizar Packs de Texturas que modifiquen las animaciones visuales de los jugadores: caminar, correr, agacharse, entre otros (incluidas las que se pueden ver en primera persona), porque puede haber bugs visuales con algunos mods.
+                </li>
+                <li className="my-5">
+                Ojo, cuando te infecte un zombi (aunque sea el efecto de nivel 1), tendrás que encontrar una de las muchas curas que se implementaron: manzana dorada, manzana encantada, botella de miel, guiso de remolacha, estofado de conejo, papa venenosa, pez tropical y pez globo (También brinda efecto de inmunidad, pero durante 2 minutos). De lo contrario morirás.
+                </li>
+                <li className="my-5">
+                Las personas que tengan cualquier tipo de pertenencias a más de la distancia establecida (10.000 bloques) del Overword, serán eliminadas. Los jugadores que encuentren esas pertenencias, se las pueden quedar. Si están o son cualquier tipo de objeto de almacenamiento reforzado o con código, pueden comunicarse con algún administrador para poder quedarse con todo el contenido dentro.
+                </li>
+                <li className="my-5">
+                Cuando estás infectado, ten cuidado al utilizar el comando /lobby. En el lobby, no aparecen mobs debido a medidas de protección. Esto significa que si mueres estando infectado mientras estás en el lobby, perderás todo tu inventario.
+                </li>
+                <li className="my-5">
+                Los aldeanos fueron modificados para que tengan un tradeo fijo. Quiere decir que, si el jugador le asigna un rol, ya no se podrá cambiar ni el rol asignado, ni los ítems que ofrece, solo se podrá subir de nivel.
+                </li>
+                <li className="my-5">
+                Las personas podrán crear un equipo de máximo 4 personas con el comando “/team create 'nombre del equipo'”. Tener un equipo tiene varios beneficios, como por ejemplo: el daño entre los miembros del equipo está desactivado, nombre del equipo visible a todos los jugadores, disponibilidad administrar el team a la disposición del creador, etc.
+                </li>
+            </ParLeft>
         </div>
     );
 }
--- src/components/Launcher.jsx
+++ src/components/Launcher.jsx
@@ -1,9 +1,27 @@
 import TitleWiki from "./react/TitleWiki";
+import ParLeft from "./react/ParLeft";
 
 export default function Launcher() {
     return (
         <div className="flex flex-col justify-center items-center">
             <TitleWiki text="Launcher" />
+            <ParLeft>
+                <li className="my-5">
+                El launcher de Kailand tiene un Anticheat, por lo que, en la carpeta de “mods” (dentro de la carpeta de “.kailand”) no podrán modificar, ni cambiar nada con relación al paquete de mods original de Kailand. Lo mismo pasa con la carpeta “resourcepacks”, pero de una manera menos estricta, para permitir que los usuarios usen sus Packs de Texturas preferidos.
+                </li>
+                <li className="my-5">
+                Los jugadores que tengan el mínimo de RAM (8 GB DRR3) se les recomienda usar todos los mods de rendimiento del Launcher en la sección de “Predeterminados”: Rubidium, FPS Reducer, Better FPS y el Rubidium Extras que se encuentra “Recomendados”.
+                </li>
+                <li className="my-5">
+                NO cambiar la versión de Java que viene configurada, ya que es la versión específica recomendada que te instala Mojang por defecto. Por lo que NO se necesita instalar ninguna otra versión de java.
+                </li>
+                <li className="my-5">
+                En el apartado “Consola” podrás ver y monitorear el Debug del Launcher, eso quiere decir que, todos los procesos que haga el Launcher y todas las opciones que clickees estarán a la vista.
+                </li>
+                <li className="my-5">
+                En el apartado de “Perfil” y en la opción “Tipo de cuenta” NO está disponible la alternativa “(Online) Microsoft”. Debido a que, hasta el momento no ha habido integración con la API de Mojang, por el momento…
+                </li>
+            </ParLeft>
         </div>
     );
 }
