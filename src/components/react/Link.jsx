092ea910c77c296ebad78a67754433b8f2a109a3
GatoArtStudio <hackhcctecno@gmail.com>
2024-07-26 20:16:19 -0500

    Agregando hover al componente de links

--- src/components/react/Link.jsx
+++ src/components/react/Link.jsx
@@ -1,6 +1,6 @@
 export default function Link({ href, text }) {
     return (
-        <a href={href} target="_blank" className="no-decorect text-blue-300 font-semibold">
+        <a href={href} target="_blank" className="no-decorect text-blue-300 font-semibold hover:text-blue-500">
             {text}
         </a>
     );
