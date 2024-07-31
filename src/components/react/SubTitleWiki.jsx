da5ad67b39f2290027bddb8ca273ba24c62be344
GatoArtStudio <hackhcctecno@gmail.com>
2024-07-31 14:08:22 -0500

    Actualizando lista de mods a la wiki

--- src/components/react/SubTitleWiki.jsx
+++ src/components/react/SubTitleWiki.jsx
@@ -1,7 +1,9 @@
 export default function SubTitleWiki({ text }) {
     return (
-        <h1 className="text-2xl font-bold mb-4 mt-7 text-left w-full hover:animate-bounce">
-            {text}
-        </h1>
+        <div className="group relative w-full">
+            <h1 className="text-2xl font-bold mb-4 mt-7 text-left w-full group-hover:animate-bounce">
+                {text}
+            </h1>
+        </div>
     );
 }
